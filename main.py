4a8f79500672b2d268b33048fd30f21aad3c9fc8
jayras <jayras@gmail.com>
2024-12-13 15:52:54 -0700

    Added character count in string

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 ################################## Functions and globals. ####################
 def main():
     path_to_file = "books/frankenstein.txt"
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        print(wordCount(file_contents))
+    book = getBookText(path_to_file)
+    print(wordCount(book))
+
+    characterCount = perCharacterCount(book)
+    print(characterCount)
+
+def getBookText(pathToBook):
+    with open(pathToBook) as f:
+        return f.read()
 
 def wordCount(input):
-    count = 0
-    lines = input.split("\n")
-    for line in lines:
-        if len(line) > 0:
-            wcount = 0
-            words  = line.split()
-            for word in words:
-                if len(word) > 0:
-                    wcount += 1
-            count += wcount
-    return count
+    return len(input.split())
+
+def perCharacterCount(input):
+    characterCount = {}
+    for char in input:
+        lc = char.lower()
+        if lc in characterCount:
+            characterCount[lc] += 1
+        else:
+            characterCount[lc] = 1
+    return characterCount
+
+    # Oh my god, so unecessary:
+    #def wordCount(input):
+    # count = 0
+    # lines = input.split("\n")
+    # for line in lines:
+    #     if len(line) > 0:
+    #         wcount = 0
+    #         words  = line.split()
+    #         for word in words:
+    #             if len(word) > 0:
+    #                 wcount += 1
+    #         count += wcount
+    # return count
+
 
 ############################################################################## 
 ### Should have nothing but main() .....
